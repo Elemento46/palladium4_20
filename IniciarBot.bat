67cc04e9c1b753c2c52d29b4dfb282e8c3b0bd1a
Alexandre Rodrigues <82237547+Elemento46@users.noreply.github.com>
2025-04-10 04:57:04 -0300

    Update IniciarBot.bat

--- IniciarBot.bat
+++ IniciarBot.bat
@@ -1,4 +1,10 @@
 @echo off
-cd /d "%~dp0dist"
-start BotTwitch.exe
-exit
+cd /d "%~dp0"
+
+:: Verifica se o executável existe
+if exist "dist\BotTwitch.exe" (
+    start dist\BotTwitch.exe
+) else (
+    echo Executável não encontrado. Certifique-se de que BotTwitch.exe está na pasta dist.
+    pause
+)
